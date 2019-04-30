d70496e6e1136fe09be5c103ac0d2df96c9da66d
RaphaelRomanet <raphael.romanet@gmail.com>
2019-04-30 04:00:52 +0200

    Remise à niveau branhce master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,8 +23,6 @@ import { StagesComponent } from './stages/stages.component';
 import { EditStagesComponent } from './edit-stages/edit-stages.component';
 import { StageService } from './services/stage.service';
 import { StageViewComponent } from './stage-view/stage-view.component';
-import { SemestreComponent } from './semestre/semestre.component';
-import { StageComponent } from './stage/stage.component';
 
 
 const appRoutes: Routes = [
@@ -39,7 +37,7 @@ const appRoutes: Routes = [
   { path: 'stages', component: StageViewComponent },
   { path: 'edit-stages', component: EditStagesComponent },
   { path: 'new-user', component: NewUserComponent },
-  { path: 'semestre', component: SemestreComponent},
+  
   { path: '**', redirectTo: 'not-found' }
 ];
 
@@ -63,17 +61,11 @@ const appRoutes: Routes = [
 
     NewUserComponent,
 
-<<<<<<< HEAD
     StagesComponent,
 
     EditStagesComponent,
 
     StageViewComponent
-=======
-    SemestreComponent,
-
-    StageComponent
->>>>>>> 7949b05852593c5cf31269c8cf117c0ffd3e826a
   ],
   imports: [
     BrowserModule,
