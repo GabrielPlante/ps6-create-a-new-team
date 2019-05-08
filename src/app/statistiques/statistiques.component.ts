3e506b5a721465e0656ea17e6a8a1bb348ecb19f
GabrielPlante <gabriel.plante@live.fr>
2019-05-08 18:28:54 +0200

    Amélioration des statistiques

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { StageService } from '../services/stage.service';
 import { Subscription } from 'rxjs';
 
@@ -8,8 +8,13 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./statistiques.component.scss']
 })
 export class StatistiquesComponent implements OnInit {
+  @Input() pays: string;
+  @Input() promotion: string;
+  @Input() type: string;
+
   stages: any[];
   stagesSubscription: Subscription;
+
 
   constructor(private stageService: StageService) { }
 
@@ -19,22 +24,28 @@ export class StatistiquesComponent implements OnInit {
         this.stages = stages;
       }
     );
+    this.pays = 'tous';
+    this.promotion = 'tous';
+    this.type = 'Stage';
     this.stageService.emitStageSubject();
   }
   addStage(pays: string,depart: Date,fin: Date,type_mobilite: string,ville: string,satisfaction: string,reussite: string,ressenti: string, promotion: string) {
     this.stageService.addStage(pays, depart, fin, type_mobilite, ville, satisfaction, reussite, ressenti, promotion);
   }
-  getReussite(pays: string, promotion: string) {
+  getReussite() {
     var i = 0;
     var reussite = 0;
     this.stages.forEach(stage => {
-      if((pays === 'tous' || pays === stage.pays) && (promotion === 'tous')){
+      if((this.pays === 'tous' || this.pays === stage.pays) && (this.promotion === 'tous' || this.promotion == stage.promotion) && this.type === stage.type_mobilite){
       i += 1;
       if (stage.reussite === 'oui'){
         reussite += 1;
       }
     }
     });
+    if (i === 0){
+      return '0 résultats';
+    }
     return reussite/i;
   }
 }
