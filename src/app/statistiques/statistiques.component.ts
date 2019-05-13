5df2330da2abed810024dbcd1f40bc1c13a59c47
AntoineBouysse <antoine.bouysse@etu.univ-cotedazur.fr>
2019-05-13 23:36:17 +0200

    conflit stage

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -48,8 +48,8 @@ export class StatistiquesComponent implements OnInit {
     this.refreshPromo();
     this.refreshStage();
   }
-  addStage(pays: string,depart: Date,fin: Date,type_mobilite: string,ville: string,satisfaction_pays: string, satisfaction_enseignement: string, satisfaction_vie: string,reussite: string,ressenti: string, promotion: string,universite:string) {
-    this.stageService.addStage(pays,depart,fin,type_mobilite,ville,satisfaction_pays,satisfaction_enseignement, satisfaction_vie ,reussite,ressenti,promotion,universite);
+  addStage(nom: string, prenom: string,pays: string,depart: Date,fin: Date,type_mobilite: string,ville: string,satisfaction_pays: string, satisfaction_enseignement: string, satisfaction_vie: string,reussite: string,ressenti: string, promotion: string,universite:string) {
+    this.stageService.addStage(nom,prenom,pays,depart,fin,type_mobilite,ville,satisfaction_pays,satisfaction_enseignement, satisfaction_vie ,reussite,ressenti,promotion,universite);
   }
 
   refreshStage(){
